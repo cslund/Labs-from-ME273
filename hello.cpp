ccb5a1d4d254333a612b090dd8d70486225fa480
cslund <77426091+cslund@users.noreply.github.com>
2021-01-13 21:35:43 -0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,24 @@
+/**************************************************
+/	Filename    : hello.cpp
+/	Author      : Christian Lund
+/	Description : Super awesome program that is super complex
+/                 #cod3rlife #yeet #c++gang #y2k #actin'a'bool
+/                 #tamaleguyrulez
+/	
+/	Revisions:
+/	Date		Author		Description
+/   Jan-12-21   Christian   started and finished so efficient
+/
+/**************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("CLS");
+    cout << "Hello World!!" << endl;
+    cout << "can i get a yeet?" << endl;
+    
+return 0;
+} 
