5fd9241b1b9ddb0ea2b217505a9149387ebe005d
cslund <77426091+cslund@users.noreply.github.com>
2021-01-13 21:36:07 -0700

    Add files via upload

---
+++ lab1main.cpp
@@ -0,0 +1,84 @@
+/**************************************************
+/	Filename    : lab1main.cpp
+/	Author      : Christian Lund
+/	Description : program should calculate the mean of 4 user inputs
+/	
+/	Revisions   :
+/	Date		Author		Description
+/   Jan 12-21   Christian   Starting out
+/   Jan 13-21   Christian   got Case 2 and 3 done?
+/
+/**************************************************/
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    //Just to clear the terminal
+    system("CLS");
+    
+    
+    /*****************************************************************************/
+
+    /*
+    //CASE 1
+    //******While trying to run CASE 1 be sure to comment out the other case codes*******
+    //set up variables that the user will input when prompted
+    float m, w, x, y, z;
+
+    //Prompt stuff to make the computer look intellegent
+    cout << "CASE 1:" << endl << endl;
+    cout << "Hello user, I will calculate the mean of four numbers of your choice!" << endl;
+    cout << "Please input the numbers you would like to use: " << endl;
+    cin >> w >> x >> y >> z;
+    //read back numbers
+    cout << "Your numbers of choice: " << w << ", " << x << ", " << y <<", and " << z << "." << endl;
+    //actual calculation
+    m = (w + x + y + z)/4;
+    cout << "The mean of your numbers is: " << m << endl;
+    */
+
+    /*****************************************************************************/
+
+    /*
+    //CASE 2
+    //******While trying to run CASE 2 be sure to comment out the other case codes*******
+    //set up variables that the user will input when prompted
+    float m;
+    int w, x, y, z;
+    //Prompt stuff to make the computer look intellegent
+    cout << "CASE 2:" << endl << endl;
+    cout << "Hello user, I will calculate the mean of four numbers of your choice!" << endl;
+    cout << "Please input the numbers you would like to use: " << endl;
+    cin >> w >> x >> y >> z;
+    //read back numbers
+    cout << "Your numbers of choice: " << w << ", " << x << ", " << y <<", and " << z << "." << endl;
+    //actual calculation
+    m = (w + x + y + z)/4;
+    cout << "The mean of your numbers is: " << m << endl;
+    */
+    
+    /*****************************************************************************/
+
+
+    
+    //CASE 3
+    //******While trying to run CASE 3 be sure to comment out the other case codes*******
+    //set up variables that the user will input when prompted
+    float m;
+    int w, x, y, z;
+    //Prompt stuff to make the computer look intellegent
+    cout << "CASE 3:" << endl << endl;
+    cout << "Hello user, I will calculate the mean of four numbers of your choice!" << endl;
+    cout << "Please input the numbers you would like to use: " << endl;
+    cin >> w >> x >> y >> z;
+    //read back numbers
+    cout << "Your numbers of choice: " << w << ", " << x << ", " << y <<", and " << z << "." << endl;
+    //actual calculation with 4.0 instead of 4
+    m = (w + x + y + z)/4.0;
+    cout << "The mean of your numbers is: " << m << endl;
+    
+    
+    return 0;
+}
